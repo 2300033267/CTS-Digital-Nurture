79752d013def90b3a368b720d174c8bb18e5447d
Nandireddy Sravanthi <163743681+2300033267@users.noreply.github.com>
2026-07-20 16:00:52 +0530

    Add files via upload

---
+++ Week3/EmployeeManagementSystem/src/test/java/com/cts/employeemanagementsystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
