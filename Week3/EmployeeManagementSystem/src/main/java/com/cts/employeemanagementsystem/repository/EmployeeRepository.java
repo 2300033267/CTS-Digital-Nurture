379cd4f339134885c17cbef95b00bbc9e926ee42
Nandireddy Sravanthi <163743681+2300033267@users.noreply.github.com>
2026-07-20 15:56:39 +0530

    Delete Week3/EmployeeManagementSystem directory

--- Week3/EmployeeManagementSystem/src/main/java/com/cts/employeemanagementsystem/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cts.employeemanagementsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cts.employeemanagementsystem.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
